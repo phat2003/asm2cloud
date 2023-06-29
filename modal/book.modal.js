bf2b02e021afafae725f4c7bc2cb363274e5790b
pham anh phat <phamanhtienphat@gmail.com>
2023-06-30 03:03:05 +0700

    first commit

--- modal/book.modal.js
+++ modal/book.modal.js
@@ -4,5 +4,6 @@ var book = new mongoose.Schema({
     description: { type: String, required: true },
     author: { type: String, required: true }
 })
+
 // Biên dịch mô hình từ schema
 module.exports = mongoose.model('books', book);
